e8e586a5aed471b246fa21212426c41907f08be2
fgsoftware1 <fgsoftwarestudio@gmail.com>
2021-01-21 21:19:53 +0000

    commit

--- src/projeto/Calendario.java
+++ src/projeto/Calendario.java
@@ -1,4 +1,37 @@
 package projeto;
 
 public class Calendario {
+    private int Dia;
+    private String Mes;
+    private int Ano;
+
+    public Calendario(){
+        Dia = 0;
+        Mes = "";
+        Ano = 0;
+    }
+
+    public int getDia(){
+        return this.Dia;
+    }
+
+    public void setDia(int dia){
+        this.Dia = dia;
+    }
+
+    public String getMes(){
+        return this.Mes;
+    }
+
+    public void setMes(String mes){
+        this.Mes = mes;
+    }
+
+    public int getAno(){
+        return this.Ano;
+    }
+
+    public void setAno(int ano){
+        this.Ano = ano;
+    }
 }
--- src/projeto/Programacao.java
+++ src/projeto/Programacao.java
@@ -1,4 +1,27 @@
 package projeto;
 
 public class Programacao {
+    private double Hora_inicio;
+    private double Hora_fim;
+
+    public Programacao(){
+        Hora_inicio = 0.0;
+        Hora_fim = 0.0;
+    }
+
+    public double getHora_inicio(){
+        return this.Hora_inicio;
+    }
+
+    public void setHora_inicio(double hora_inicio){
+        this.Hora_inicio = hora_inicio;
+    }
+
+    public double getHora_fim(){
+        return this.Hora_fim;
+    }
+
+    public void setHora_fim(double hora_fim){
+        this.Hora_fim = hora_fim;
+    }
 }
